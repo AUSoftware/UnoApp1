96b35939ab9ab699d2e9db95c38f866a9f46f7a3
quyenn <4618634+qnguyen12@users.noreply.github.com>
2023-11-30 11:10:04 +1100

    Initial commit

---
+++ UnoApp1/UnoApp1/Presentation/SignUpPage.xaml.cs
@@ -0,0 +1,11 @@
+namespace UnoApp1.Presentation;
+/// <summary>
+/// An empty page that can be used on its own or navigated to within a Frame.
+/// </summary>
+public sealed partial class SignUpPage : Page
+{
+    public SignUpPage()
+    {
+        this.InitializeComponent();
+    }
+}
---
+++ UnoApp1/UnoApp1/Presentation/SignUpViewModel.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnoApp1.Presentation;
+public class SignUpViewModel
+{
+    public SignUpViewModel()
+    {
+        
+    }
+}
