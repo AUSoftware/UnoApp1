a4e0677299e77cf3489711978133278ccd149b7e
quyenn <4618634+qnguyen12@users.noreply.github.com>
2023-11-30 11:29:10 +1100

    splash screen update

--- UnoApp1/UnoApp1/App.cs
+++ UnoApp1/UnoApp1/App.cs
@@ -63,21 +63,6 @@ public class App : Application
 
     private static void RegisterRoutes(IViewRegistry views, IRouteRegistry routes)
     {
-        //views.Register(
-        //    new ViewMap(ViewModel: typeof(ShellViewModel)),
-        //    new ViewMap<MainPage, MainViewModel>(),
-        //    new DataViewMap<SecondPage, SecondViewModel, Entity>()
-        //);
-
-        //routes.Register(
-        //    new RouteMap("", View: views.FindByViewModel<ShellViewModel>(),
-        //        Nested: new RouteMap[]
-        //        {
-        //            new RouteMap("Main", View: views.FindByViewModel<MainViewModel>()),
-        //            new RouteMap("Second", View: views.FindByViewModel<SecondViewModel>()),
-        //        }
-        //    )
-        //);
         views.Register(
             new ViewMap(ViewModel: typeof(ShellViewModel)),
             new ViewMap<SignUpPage, SignUpViewModel>(),
